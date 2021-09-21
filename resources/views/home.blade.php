1292bdc935f6a948a08292ffcf5f6e9db8fec606
HaseebHassan <mhhassan095@gmail.com>
2021-09-20 17:29:02 -0700

    home page gui fixed

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -458,6 +458,26 @@
         </div>
     </div>
 </section>
+<section class="contact headline">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6 ">
+                <div class="map">
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d13004082.928417291!2d-104.65713107818928!3d37.27557827818067!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x54eab584e432360b%3A0x1c3bb99243deb742!2sUnited%20States!5e0!3m2!1sen!2s!4v1621967520369!5m2!1sen!2s" width="100%" height="660" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+                </div>
+            </div>
+            <div class="col-lg-6 dis-flex">
+                <h2 class="heading-two mb5">Contact Us <span>Contact Information</span></h2>
+                <div class="content">
+                <h2 class="heading-two mb5">Our Location <span>44 Lorem Ave. West Dummy, IL 80155</span></h2>
+                <h2 class="heading-two mb5"> phone number<span> +1 8049 432</span></h2>
+                <h2 class="heading-two mb5"> email address<span> loremipsum@dummy.com</span></h2>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
 
 <!-- Teacher -->
 
